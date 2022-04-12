4f70c87684d4a40377981ab9b9b5039e743903ce
ferain <ferain@kakao.com>
2022-04-12 22:37:29 +0900

    create

---
+++ backEnd/clientTest/src/test/java/com/example/clientTest/ClientTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clientTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
