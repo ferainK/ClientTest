769cdbae5ef387d68902d3ac51bb54b6d7612753
ferain <ferain@kakao.com>
2022-04-21 23:32:10 +0900

    이슈사항

---
+++ restaurant/src/main/java/com/example/restaurant/wishList/entity/WishListEntity.java
@@ -0,0 +1,22 @@
+package com.example.restaurant.wishList.entity;
+
+import com.example.restaurant.db.DbEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class WishListEntity extends DbEntity {
+  private String title;                 // 가게명
+  private String category;              // 카테고리
+  private String lotAddress;            // 지번 주소
+  private String roadAddress;           // 도로명 주소
+  private String homePage;              // 홈페이지 주소
+  private String image;                 // 관련 이미지
+  private boolean isVisit;              // 방문 여부
+  private int visitCount;               // 방문 횟수
+  private LocalDateTime lastVisitDate;  // 최근 방문일
+}
---
+++ restaurant/src/main/java/com/example/restaurant/wishList/repository/WishListRepository.java
@@ -0,0 +1,10 @@
+package com.example.restaurant.wishList.repository;
+
+import com.example.restaurant.db.DbRepositoryAbstract;
+import com.example.restaurant.wishList.entity.WishListEntity;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class WishListRepository extends DbRepositoryAbstract<WishListEntity> {
+
+}
